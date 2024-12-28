c2acfd3ee055f4b2f0d51833592bb2ea21b7fe65
Edward Ofori <tetraed.llc@gmail.com>
2024-12-28 02:18:07 -0500

    Edited DB index file to include host and port2

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -8,7 +8,7 @@ import { TicketFactory } from './ticket.js';
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-      host: 'localhost' || '127.0.0.1',
+      host: '127.0.0.1',
       dialect: 'postgres',
       port: 5432,
       dialectOptions: {
